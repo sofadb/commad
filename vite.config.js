f037554c20bf7e5c686842463579e00f1a6fc53b
Aitect <lean-architect@proton.me>
2025-08-05 07:04:33 +0000

    feat: add gh-pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/commad/',
   optimizeDeps: {
     allowNodeBuiltins: ['pouchdb-browser', 'pouchdb-utils']
   },
